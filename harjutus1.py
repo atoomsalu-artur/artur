1d658e85e6c0b1e8a7a80505c9c0c52eb55d5b57
atoomsalu-artur <atoomsalu@hkhk.edu.ee>
2025-10-09 14:54:54 +0300

    Add files via upload

--- harjutus1.py
+++ harjutus1.py
@@ -1,1 +1,39 @@
-print("Tere Maailm!")
+import turtle
+
+#kolmnurk
+turtle.penup()
+turtle.goto(-400 ,200)
+turtle.pendown()
+
+#kolmnurk
+for i in range(3): #tsukkel 3 korda
+    turtle.fd(200) #fd on ette
+    turtle.lt(120) #lt on vasakule
+    
+#liigu kuhugi
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+
+#ruut
+for i in range(4):
+    turtle.fd(150)
+    turtle.lt(90)
+
+import turtle
+# 
+turtle.left(75)
+turtle.forward(150)
+# 
+turtle.right(150)
+turtle.forward(150)
+# 
+turtle.backward(75)
+turtle.left(75)
+turtle.forward(-35)
+
+
+
+    
+     
+     
