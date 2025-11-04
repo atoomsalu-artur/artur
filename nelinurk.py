da9cedfb85f8f57b2fe3cce083c6318b29275b1b
atoomsalu-artur <atoomsalu@hkhk.edu.ee>
2025-11-04 15:35:15 +0200

    Add files via upload

---
+++ nelinurk.py
@@ -0,0 +1,20 @@
+import turtle
+
+t = turtle.Turtle()
+
+for _ in range(4):
+    t.forward(100)
+    t.right(90)
+
+t.penup()
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.left(90)
+t.pendown()
+
+for _ in range(4):
+    t.forward(100)
+    t.right(90)
+
+turtle.done()
